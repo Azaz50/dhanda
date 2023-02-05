5c2626a62c23b2b8d9ffa7dcfee39478acb6e03e
Azaz50 <azazmohammad14@gmail.com>
2023-02-05 21:16:50 +0530

    party_created() updated

--- src/party_create.c
+++ src/party_create.c
@@ -1,43 +1,5 @@
 #include<dhanda/dhanda.h>
-#include<dhanda/party.h>
 
-int party_add(dhanda *app, party *party)
-{
-     struct party p;
-     char local_sql[1024];
-     char *err = 0;
-     int ret;
-      
-    sprintf(local_sql, "INSERT INTO parties(fname, lname, phone, amount) VALUES('%s', '%s', '%ld', '%ld')",
-                                                   "party->fname, party->lname, party->phone, party->amount)";
-                           
-    ret = sqlite3_exec(db, local_sql, NULL, NULL, &err);
-
-     if (ret != SQLITE_OK) {
-          fprintf(stderr, "sqlite3_exec: %s\n", err);
-          return -1;
-     } else {
-          return 0;
-     }   
-   
-   char *sql =  "INSERT INTO 'parties' ADD COLUMN
-                (
-                   'created_at' DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                   'updated_at' DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-    
-                )
-                 AFTER 'amount';"
-
-    sprintf(local_sql, sql, party->cat, party->uat);
-
-     if (ret != SQLITE_OK) {
-          fprintf(stderr, "sqlite3_exec: %s\n", err);
-          return -1;
-     } else {
-          return 0;
-     }   
-
-}
 
 int party_add(dhanda *app, party *party)
 {
