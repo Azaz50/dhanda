fe828f56d5e893f46875b14245dd0c7c4e0826c3
Azaz50 <azazmohammad14@gmail.com>
2023-02-08 21:25:17 +0530

    party_delete is modified

--- src/party_delete.c
+++ src/party_delete.c
@@ -15,13 +15,13 @@ int party_delete(dhanda *app, party *party)        // return 1 for succes
 
 
 	
-	sprintf(local_sql, "DELETE FROM party WHERE id = %d", party->id);
+	sprintf(local_sql, "DELETE FROM parties WHERE id = %d", party->id);
 
 	ret = sqlite3_exec(app->db, local_sql, NULL, NULL, &err);
 
     if (ret != SQLITE_OK) {
         fprintf(stderr, "party_delete(): sqlite3_exec error: %s\n", err);
-        ret = -1;
+        return -1;
     } 
 
     return 0;
