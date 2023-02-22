6042838c165629b4352e457f900faaa7bf23fc7e
Azaz50 <azazmohammad14@gmail.com>
2023-02-22 18:57:11 +0530

    for functions declarations done here

---
+++ src/include/dhanda/api.h
@@ -0,0 +1,5 @@
+void init_app(void);
+int api_party_add(struct http_request *req);
+int api_txn_add(struct http_request *req);
+
+
---
+++ src/include/dhanda/api_util.h
@@ -0,0 +1,2 @@
+struct kore_json_item * party_struct_to_korejson(struct party *p);
+struct kore_json_item * txn_struct_to_korejson(struct txn *t);
