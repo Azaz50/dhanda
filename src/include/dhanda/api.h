03df5de92248a9a6a8923651672d728933e5661c
Azaz50 <azazmohammad14@gmail.com>
2023-02-26 13:28:02 +0530

    functuions declaration are done here for api.c

--- src/include/dhanda/api.h
+++ src/include/dhanda/api.h
@@ -1,5 +1,12 @@
 void init_app(void);
 int api_party_add(struct http_request *req);
 int api_txn_add(struct http_request *req);
+int api_party_get(struct http_request *req);
+int api_txn_get(struct http_request *req);
+int api_party_update(struct http_request *req);
+int api_party_delete(struct http_request *req);
+
+
+//int api_party_details(struct http_request *req);
 
 
