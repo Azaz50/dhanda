e08991442a3cc59d8cc2540af8f44efa6db94f25
Azaz50 <azazmohammad14@gmail.com>
2023-06-12 09:05:30 +0530

    adding and removing some function declaration

--- src/include/dhanda/api_util.h
+++ src/include/dhanda/api_util.h
@@ -1,9 +1,9 @@
-struct kore_json_item * party_struct_to_korejson(struct party *p);
-struct kore_json_item * txn_struct_to_korejson(struct txn *t);
-struct kore_json_item * party_list_to_korejson(struct list *parties);
-struct kore_json_item * party_list_to_korejson(struct list *parties);
-struct kore_json_item * txn_list_to_korejson(struct list *transaction);
-int kore_apputil_extract_route_ids(const char *path, ...);
+int
+kore_apputil_extract_route_ids(const char *path, ...);
 
+int
+util_http_json_error_response(struct http_request *req, struct kore_json_item *item, const char *field, int (*validator)(char *));
 
+int
+util_http_response_msg(struct http_request *req, char *str);
 
